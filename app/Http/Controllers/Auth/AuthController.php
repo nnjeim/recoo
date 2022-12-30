82b3d3e2e0d48fe8ffd37def3d898b7b2f84377b
Najm Njeim <najm@njeim.net>
2022-12-30 06:25:39 +0000

    modified auth login controller and methods

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginUsingCredentialsRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\Factory as ViewFactory;
+use Illuminate\Http\RedirectResponse;
+
+class AuthController extends Controller
+{
+	private ViewFactory $viewFactory;
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct(ViewFactory $viewFactory)
+	{
+		$this->middleware('guest')->except('logout');
+
+		$this->viewFactory = $viewFactory;
+	}
+
+    /**
+     * Display the login view.
+     *
+     * @return View
+     */
+    public function showLoginForm(): View
+    {
+		if (! session()->has('url.intended')) {
+			$url = str_contains(url()->previous(), config('app.url'))
+				? url()->previous()
+				: RouteServiceProvider::HOME;
+
+			session(['url.intended' => $url]);
+		}
+
+		return $this
+			->viewFactory
+			->make('auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     *
+     * @param  LoginUsingCredentialsRequest  $request
+     * @return RedirectResponse
+     */
+    public function LoginUsingCredentials(LoginUsingCredentialsRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return redirect()->intended(RouteServiceProvider::HOME);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     *
+     * @param  Request  $request
+     * @return RedirectResponse
+     */
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
--- routes/partials/auth.php
+++ routes/partials/auth.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
@@ -15,10 +14,6 @@ Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
-
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
-
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
 
@@ -45,6 +40,4 @@ Route::middleware('auth')->group(function () {
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
